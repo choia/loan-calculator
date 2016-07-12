f715e5fc776c3f7307e53fce88c0768730eb9f70
choia <alex.ji.choi@gmail.com>
2016-07-11 22:31:35 -0400

    Added text_updated function

--- loan_calculator.py
+++ loan_calculator.py
@@ -1,8 +1,7 @@
 import sys
-from PyQt5.QtCore import Qt, pyqtSignal, pyqtSlot
 from PyQt5.QtGui import QIntValidator, QDoubleValidator
-from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit, QSpinBox,
-                             QVBoxLayout, QApplication)
+from PyQt5.QtWidgets import QWidget, QApplication, QVBoxLayout, QLineEdit, QLabel
+
 
 class Window(QWidget):
 
@@ -11,87 +10,56 @@ class Window(QWidget):
         self.init_ui()
 
     def init_ui(self):
-        self.principle = QLineEdit()
-        self.principle.setValidator(QDoubleValidator(-1, 99.99, 2))
-        self.rate = QLineEdit()
-        self.rate.setValidator(QDoubleValidator(-1, 99.99, 2))
-        self.number_of_years = QSpinBox()
-        self.number_of_years.setRange(1, 99)
-        self.number_of_years.setValue(5)
-        self.number_of_payment_per_years = QSpinBox()
-        self.number_of_payment_per_years.setRange(1, 99)
-        self.number_of_payment_per_years.setValue(12)
+        self.setGeometry(300,300,300,300)
+        self.setWindowTitle("Test")
+        self.qline1 = QLineEdit()
+        self.qline1.setValidator(QDoubleValidator(0.0, 999999.99, 2))
+        self.qline2 = QLineEdit()
+        self.qline2.setValidator(QDoubleValidator(0.0, 999999.99, 2))
+        self.qline3 = QLineEdit()
+        self.qline3.setValidator(QIntValidator())
+        self.qline4 = QLineEdit()
+        self.qline4.setValidator(QIntValidator())
 
-        self.label1 = QLabel()
+        self.qline1.setPlaceholderText("Enter your number!")
+        self.qlabel1 = QLabel(self)
 
         vbox = QVBoxLayout()
-        vbox.addWidget(self.principle)
-        vbox.addWidget(self.rate)
-        vbox.addWidget(self.number_of_years)
-        vbox.addWidget(self.number_of_payment_per_years)
-        vbox.addWidget(self.label1)
-
+        vbox.addWidget(self.qline1)
+        vbox.addWidget(self.qline2)
+        vbox.addWidget(self.qline3)
+        vbox.addWidget(self.qline4)
+        vbox.addWidget(self.qlabel1)
+        vbox.addStretch(1)
         self.setLayout(vbox)
 
+        self.qline1.textEdited.connect(self.text_updated)
+        self.qline2.textEdited.connect(self.text_updated)
+        self.qline3.textEdited.connect(self.text_updated)
+        self.qline4.textEdited.connect(self.text_updated)
 
-        self.principle.editingFinished.connect(self.loan_calculate)
-        self.rate.editingFinished.connect(self.loan_calculate)
-
-        self.number_of_years.editingFinished.connect(self.loan_calculate)
-        self.number_of_payment_per_years.editingFinished.connect(self.loan_calculate)
-
-        #self.principle.textChanged.connect(self.loan_calculate)
-        #self.rate.textChanged.connect(self.loan_calculate)
-
-        self.number_of_years.valueChanged.connect(self.loan_calculate)
-        self.number_of_payment_per_years.valueChanged.connect(self.loan_calculate)
-
-
-        self.setGeometry(600, 400, 400, 400)
-        self.setWindowTitle('Test')
-
-    def loan_calculate(self):
-        if self.principle.isModified() and self.rate.isModified():
-
-
-            p = float(self.principle.text())
-            r = float(self.rate.text())
-            y = int(self.number_of_years.text())
-            n = int(self.number_of_payment_per_years.text())
-
-            if r == 0:
-                monthly_payment = self.rate_is_zero_calculate(p, y, n)
-
-            else:
-                monthly_payment = self.calculate(p, r, y, n)
-
-            self.label1.setText("{:.2f}".format(monthly_payment))
-
-
-    def rate_is_zero_calculate(self, p, y, n):
-        total_number_payment = y * n
-        monthly_payment = p / total_number_payment
-
-        return monthly_payment
+    def text_updated(self):
+        if self.qline1.text() and self.qline2.text() and self.qline3.text() and self.qline4.text():
+            print('yes')
+            q1 = float(self.qline1.text())
+            q2 = float(self.qline2.text())
+            q3 = int(self.qline3.text())
+            q4 = int(self.qline4.text())
+            result = self.calculate(q1, q2, q3, q4)
+            total = format(result, '.2f')
+            self.qlabel1.setText(str(total))
 
     def calculate(self, p, r, y, n):
         total_number_payment = y * n
         calculated_rate = r / 100 / n
         formula_one = calculated_rate * ((calculated_rate + 1) ** total_number_payment)
         formula_two = ((1 + calculated_rate) ** total_number_payment) - 1
-        monthly_payment = p * (formula_one / formula_two)
-        return monthly_payment
-
-#    def calculate(self, text):
-#        if text:
-#            text = (int(text) * 2)
-#            self.line2.setText(str(text))
-#        else: self.line2.setText('')
+        result = p * (formula_one / formula_two)
+        return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
     window.show()
     sys.exit(app.exec_())
-
